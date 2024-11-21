1f092996dae21cae0f95032990076e7da638c901
zm68526 <104472530+zm68526@users.noreply.github.com>
2024-11-21 17:03:56 -0500

    Implemented signing up & logging in
    Code is very messy rn, sorry about that. If it's causing problems later I'll go back and rewrite. In the LoginActivity/SignupActivity classes I added a comment where I think we could transition to the new activity.

---
+++ app/src/main/java/edu/uga/cs/cartcrew/LoginActivity.java
@@ -0,0 +1,81 @@
+package edu.uga.cs.cartcrew;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+    TextView textView;
+    Button button;
+    EditText emailField;
+    EditText passwordField;
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        mAuth = FirebaseAuth.getInstance();
+
+        textView = findViewById(R.id.textView2);
+        textView.setText("Login");
+
+        emailField = findViewById(R.id.editTextTextEmailAddress);
+        passwordField = findViewById(R.id.editTextTextPassword);
+
+        button = findViewById(R.id.mainButton);
+        button.setText("Login");
+        button.setOnClickListener(v -> {
+            String enteredEmail = emailField.getText().toString();
+            String enteredPassword = passwordField.getText().toString();
+            mAuth.signInWithEmailAndPassword( enteredEmail, enteredPassword )
+                    .addOnCompleteListener(this, new AuthenticationHandler());
+        });
+    }
+
+    class AuthenticationHandler implements OnCompleteListener<AuthResult> {
+        @Override
+        public void onComplete(@NonNull Task<AuthResult> task) {
+            if (task.isSuccessful()) {
+                FirebaseUser user = mAuth.getCurrentUser();
+                // launch main activity with user here?
+            }
+            else {
+                showError(task.getException());
+            }
+        }
+    }
+
+    private void showError(Exception e) {
+        Toast toast = Toast.makeText(this.getApplicationContext(),
+                "Authentication Failed: " + (e == null ? "" : e.getMessage()),
+                Toast.LENGTH_SHORT);
+        toast.show();
+    }
+}
